7fe8444ab2c2788a90a06d0d2b19178d53da6304
Arkent Nathanael <arkent.huang@gmail.com>
2020-07-16 16:45:31 +0700

    day 4 finale

--- trainingday3/express-demo/index.js
+++ trainingday3/express-demo/index.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+const courses =[
+    {id:1, name:'course1'},{id:2, name:'course2'},{id:3, name:'course3'}
+];
 
 app.get("/", (req, res) =>{
     res.send('Hello World dengan nodemon');
 });
 
 app.get("/api/courses", (req, res) =>{
-    res.send([1,2,3,4, "ini lima"])
+    res.send(courses)
 });
 
 app.get("/api/courses/:id/:name", (req, res)=>{
     res.send(req.params.id+ " " +req.params.name);
-})
+});
+
+app.get("/api/courses/:id", (req,res)=>{
+    const course = courses.find(c=> c.id === parseInt(req.params.id) );
+    if(!course) res.status(404).send('The course with the given ID was not found.');
+
+    res.send(course);
+    res.send(req.query);
+    // var parameter_sort = req.query.sortBy;
+    // res.send(parameter_sort);
+});
+
 // PORT
 const port = process.env.PORT || 3000;
 
@@ -19,6 +34,14 @@ app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
 });
 
+app.post("/api/courses/", (req,res) =>{
+    const course = {
+        id: courses.length+1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
 
 /*const http = require('http');
 const server = http.createServer((req, res) =>{
