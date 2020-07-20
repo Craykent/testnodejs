a76fcc5d530a6ab4b91712533d2e4c98b51d6287
Arkent Nathanael <arkent.huang@gmail.com>
2020-07-20 12:59:13 +0700

    Friday

--- trainingday3/express-demo/index.js
+++ trainingday3/express-demo/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const Joi = require('joi');
 app.use(express.json());
+
 const courses =[
     {id:1, name:'course1'},{id:2, name:'course2'},{id:3, name:'course3'}
 ];
 
+// RECEIVE DATA
 app.get("/", (req, res) =>{
     res.send('Hello World dengan nodemon');
 });
@@ -34,7 +37,20 @@ app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
 });
 
+// ADD 1 NEW DATA    const course = {
 app.post("/api/courses/", (req,res) =>{
+
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    const result = schema.validate(req.body);
+    console.log(result);
+
+        if(result.error){
+            return res.status(400).send(result.error.details[0].message)
+        }
+
     const course = {
         id: courses.length+1,
         name: req.body.name
@@ -42,6 +58,42 @@ app.post("/api/courses/", (req,res) =>{
     courses.push(course);
     res.send(course);
 });
+
+app.put("/api/courses/:id", (req,res)=> {
+    const {error} = validateCourse(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message)
+    }
+
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found.');
+
+
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete("/api/courses/:id", (req,res)=>{
+
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found.');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+
+});
+
+function validateCourse(course){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return schema.validate(course);
+}
+
 
 /*const http = require('http');
 const server = http.createServer((req, res) =>{
